c9149f9be260da6e952fbeccdd5eed1e23a6e05b
smnrkzl <kizilsemanur@gmail.com>
2024-04-02 00:55:11 +0300

    Step1

---
+++ Step1/src/rentACar/RentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Brand:Entity<Guid>
+    {
+        public string Name { get; set; }
+        public Brand()
+        {
+            
+        }
+
+        public Brand(Guid id, string name)
+        {
+            id = id;
+            Name = name;
+        }
+    }
+}
