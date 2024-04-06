f8aedb00132d2d7022a5a15fd5f3f59f19d8c930
Sema Nur Kızıl <96079063+smnrkzl@users.noreply.github.com>
2024-04-06 18:54:30 +0300

    Update BaseController.cs

--- Step1/src/rentACar/RentACar/WebApi/Controllers/BaseController.cs
+++ Step1/src/rentACar/RentACar/WebApi/Controllers/BaseController.cs
@@ -11,3 +11,16 @@ namespace WebApi.Controllers
 
     }
 }
+
+/* public class BaseController : ControllerBase: Bu satırda BaseController adında bir sınıf tanımlanıyor.
+Bu sınıf, ASP.NET Core uygulamalarında diğer controller sınıflarının temelini oluşturur. ControllerBase sınıfı,
+ASP.NET Core’un temel controller sınıfıdır ve HTTP isteklerini işlemek için kullanılır.*/
+
+/*private IMediator? _mediator;: Bu satırda _mediator adında bir özel (private) alan (field) tanımlanıyor.
+_mediator değişkeni, mediator pattern tasarım deseni için kullanılır. Mediator, nesneler arasındaki iletişimi kolaylaştırmak için kullanılır.
+Bu örnekte, _mediator alanı muhtemelen uygulamanın içindeki farklı bileşenler arasında iletişim kurmak için kullanılıyor.*/
+
+/*protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();: Bu satırda Mediator adında
+bir property (özellik) tanımlanıyor. Bu özellik, _mediator alanını döndürür. Eğer _mediator henüz atanmamışsa,
+HttpContext.RequestServices.GetService<IMediator>() ifadesi ile bir IMediator nesnesi alınır ve _mediator alanına atanır.
+Bu yapı, lazy initialization (geç başlatma) prensibini kullanır.*/
