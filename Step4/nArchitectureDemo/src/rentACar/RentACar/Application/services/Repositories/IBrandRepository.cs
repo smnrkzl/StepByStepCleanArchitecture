b5ed14cb3897b11ca1a904e3b26a42f115be9978
smnrkzl <kizilsemanur@gmail.com>
2024-05-04 18:38:29 +0300

    step4

---
+++ Step4/nArchitectureDemo/src/rentACar/RentACar/Application/services/Repositories/IBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.services.Repositories
+{
+    public interface IBrandRepository:IAsyncRepository<Brand>,IRepository<Brand>
+    {
+
+    }
+}
