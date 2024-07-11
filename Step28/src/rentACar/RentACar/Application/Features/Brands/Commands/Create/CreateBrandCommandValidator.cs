aae7c3d93136f52b0319cf54f82c579b40d4cecd
smnrkzl <kizilsemanur@gmail.com>
2024-07-11 12:56:53 +0300

    add Step28

---
+++ Step28/src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommandValidator:AbstractValidator<CreateBrandCommand>
+{
+
+
+}
