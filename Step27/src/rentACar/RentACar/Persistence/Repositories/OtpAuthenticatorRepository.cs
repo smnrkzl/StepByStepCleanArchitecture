bb05ada6d3f7f6df2cc96c5859eac8f4ffbab277
smnrkzl <kizilsemanur@gmail.com>
2024-07-11 12:34:44 +0300

    add Step27

---
+++ Step27/src/rentACar/RentACar/Persistence/Repositories/OtpAuthenticatorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories;
+
+public class OtpAuthenticatorRepository : EfRepositoryBase<OtpAuthenticator, int, BaseDbContext>, IOtpAuthenticatorRepository
+{
+    public OtpAuthenticatorRepository(BaseDbContext context)
+      : base(context) { }
+}
